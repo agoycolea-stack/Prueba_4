2e6e978a2f911b72a7238df917327eb6abef653a
Agustin Goycolea <agoycolea@fen.uchile.cl>
2020-08-28 21:34:56 -0400

    segundo intento de prueba

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,6 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :retweets, dependent: :destroy
-
+  validates :content, presence: true
   paginates_per 50
 end
